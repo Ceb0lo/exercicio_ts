a4b5090166efea21ce6a251513278530456d2187
Erick Silva <ericklemos2001@hotmail.com>
2025-02-18 10:06:53 -0300

    Add TypeScript

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicacaoA(numeroA:number, numeroB: number){
+    return numeroA * numeroB
+}
+
+const multiplicacaoB = (numeroA:number, numeroB: number): number => numeroA * numeroB
+
+function dizOlaA(nome:string) {
+    return `Olá ${nome}`
+}
+
+const dizOlaB = (nome:string):string => `Olá ${nome}`
